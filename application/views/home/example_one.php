5a141a3ebd25956b206a2e2facd8ffde8940427c
Panique <panique@web.de>
2013-12-14 21:44:50 +0100

    deleted entire project to prepare full merge with "advanced branch"

--- application/views/home/example_one.php
+++
@@ -1,8 +0,0 @@
-<div class="container">
-    <!-- Info -->
-    <div class="where-are-we-box">
-        Everything in this box is loaded from <span class="bold">application/views/home/example_one.php</span> !
-    </div>
-    <h1>The view</h1>
-    <p>In a real application this could be an page that contains whatever you want.</p>
-</div>
--- application/views/home/index.php
+++
@@ -1,8 +0,0 @@
-<div class="container">
-    <!-- Info -->
-    <div class="where-are-we-box">
-        Everything in this box is loaded from <span class="bold">application/views/home/index.php</span> !
-    </div>
-    <h1>The view</h1>
-    <p>In a real application this could be the homepage.</p>
-</div>
