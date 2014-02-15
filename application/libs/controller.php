2179ee98902c64ece5fdf087b5b71fca75f7c455
Alex Mosolov <mosolov.dev@gmail.com>
2014-02-15 23:00:29 +0400

    classname.php to ClassName.php

--- application/libs/controller.php
+++ application/libs/controller.php
@@ -45,7 +45,7 @@ class Controller
      */
     public function loadModel($model_name)
     {
-        require 'application/models/' . strtolower($model_name) . '.php';
+        require 'application/models/' . $model_name . '.php';
         // return new model (and pass the database connection to the model)
         return new $model_name($this->db);
     }
