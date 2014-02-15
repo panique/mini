1f92172676c1ce96227d5c5e77d4622713c598c2
Alex Mosolov <mosolov.dev@gmail.com>
2014-02-15 13:46:14 +0400

    Transfer base part of the implementation of the model in a separate class

---
+++ application/libs/model.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This is the "base model class". All other "real" models extend this class.
+ */
+class Model
+{
+    /**
+     * Every model needs a database connection, passed to the model
+     * @param object $db A PDO database connection
+     */
+    function __construct($db) {
+        try {
+            $this->db = $db;
+        } catch (PDOException $e) {
+            exit('Database connection could not be established.');
+        }
+    }
+}
--- application/models/songsmodel.php
+++ application/models/songsmodel.php
@@ -1,19 +1,7 @@
 <?php
 
-class SongsModel
+class SongsModel extends Model
 {
-    /**
-     * Every model needs a database connection, passed to the model
-     * @param object $db A PDO database connection
-     */
-    function __construct($db) {
-        try {
-            $this->db = $db;
-        } catch (PDOException $e) {
-            exit('Database connection could not be established.');
-        }
-    }
-
     /**
      * Get all songs from database
      */
