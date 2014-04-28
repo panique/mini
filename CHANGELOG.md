23d29524df503ce482181d93d2b71eae8ab3bfb5
Panique <panique@web.de>
2014-04-28 20:19:13 +0200

    jQuery update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 CHANGE LOG
 ==========
 
+** April 28th 2014 **
+- updated jQuery link to 2.1
+
 **January 4th 2014**
 - fixed htaccess issue when there's a controller named "index" and a base index.php (which collide)
 
--- application/views/_templates/header.php
+++ application/views/_templates/header.php
@@ -9,7 +9,7 @@
     <!-- css -->
     <link href="<?php echo URL; ?>public/css/style.css" rel="stylesheet">
     <!-- jQuery -->
-    <script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
+    <script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
     <!-- our JavaScript -->
     <script src="<?php echo URL; ?>public/js/application.js"></script>
 </head>
