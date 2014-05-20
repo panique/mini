bf37438cbfa9f2efe0484eaefa9dda5ddf54635a
AD7six <andydawson76@gmail.com>
2014-05-20 16:29:38 +0000

    Rewrite all requests to the public folder.
    This means that users can choose between:
    
     * Drop in install. http://localhost/php-mvc **just works**
     * Drop in install. http://localhost/ **just works** if in the document
     * root
     * Create a vhost pointing at the public folder
    
    This removes the rewrite base which is unnecessary in most installs. and
    prevents the possibility of putting the code in a different place or
    even renaming the folder
    
    fixes #79

--- application/libs/application.php
+++ application/libs/application.php
@@ -27,11 +27,11 @@ class Application
         $this->splitUrl();
 
         // check for controller: does such a controller exist ?
-        if (file_exists('./application/controller/' . $this->url_controller . '.php')) {
+        if (file_exists(APP . 'controller/' . $this->url_controller . '.php')) {
 
             // if so, then load this file and create this controller
             // example: if controller would be "car", then this line would translate into: $this->car = new car();
-            require './application/controller/' . $this->url_controller . '.php';
+            require APP . 'controller/' . $this->url_controller . '.php';
             $this->url_controller = new $this->url_controller();
 
             // check for method: does such a method exist in the controller ?
@@ -57,7 +57,7 @@ class Application
             }
         } else {
             // invalid URL, so simply show home/index
-            require './application/controller/home.php';
+            require APP . 'controller/home.php';
             $home = new Home();
             $home->index();
         }
--- public/index.php
+++ public/index.php
@@ -10,17 +10,19 @@
  * @license http://opensource.org/licenses/MIT MIT License
  */
 
+define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
+define('APP', ROOT . 'application' . DIRECTORY_SEPARATOR);
 // load the (optional) Composer auto-loader
-if (file_exists('vendor/autoload.php')) {
-    require 'vendor/autoload.php';
+if (file_exists(ROOT . 'vendor/autoload.php')) {
+    require ROOT . 'vendor/autoload.php';
 }
 
 // load application config (error reporting etc.)
-require 'application/config/config.php';
+require APP . '/config/config.php';
 
 // load application class
-require 'application/libs/application.php';
-require 'application/libs/controller.php';
+require APP . '/libs/application.php';
+require APP . '/libs/controller.php';
 
 // start the application
 $app = new Application();
