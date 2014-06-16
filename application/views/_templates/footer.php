cf12a7b7eee0f51c9103414d06c3ff43a1b9d315
digitaltoast <digitaltoast@gmail.com>
2014-06-16 11:30:56 +0100

    Remove protocol from external script includes
    Removed protocol from script src as per http://www.paulirish.com/2010/the-protocol-relative-url/ so that scripts actually work when site is loaded over an https connection. Also, jquery 2+ won't work on IE<9 which might confuse some newbies wondering why the javascript wasn't coming up. Once they've got the site working, they can easily switch to the 2.x jQuery branch.

--- application/views/_templates/footer.php
+++ application/views/_templates/footer.php
@@ -1,5 +1,5 @@
     <!-- jQuery -->
-    <script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     <!-- our JavaScript -->
     <script src="<?php echo URL; ?>public/js/application.js"></script>
 </body>
