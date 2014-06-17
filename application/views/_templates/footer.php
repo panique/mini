6b2ba4c8522f585b331d6558a1624b5311454665
Panique <panique@web.de>
2014-06-17 15:29:53 +0200

    explained the way jQuery is loaded

--- application/views/_templates/footer.php
+++ application/views/_templates/footer.php
@@ -1,5 +1,5 @@
-    <!-- jQuery -->
-    <!-- the way it is loaded here (protocol-less) is totally correct and recommended -->
+    <!-- jQuery, loaded in the recommended protocol-less way -->
+    <!-- more http://www.paulirish.com/2010/the-protocol-relative-url/ -->
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     <!-- our JavaScript -->
     <script src="<?php echo URL; ?>public/js/application.js"></script>
