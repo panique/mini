91afffbd12a83bd474e5b0672281a80988a99892
Panique <panique@web.de>
2014-11-09 02:02:26 +0100

    renamed folders

--- application/core/application.php
+++ application/core/application.php
@@ -27,12 +27,12 @@ class Application
             $page = new Home();
             $page->index();
 
-        } elseif (file_exists(APP . 'controller/' . $this->url_controller . '.php')) {
+        } elseif (file_exists(APP . 'controllers/' . $this->url_controller . '.php')) {
             // here we did check for controller: does such a controller exist ?
 
             // if so, then load this file and create this controller
             // example: if controller would be "car", then this line would translate into: $this->car = new car();
-            require APP . 'controller/' . $this->url_controller . '.php';
+            require APP . 'controllers/' . $this->url_controller . '.php';
             $this->url_controller = new $this->url_controller();
 
             // check for method: does such a method exist in the controller ?
