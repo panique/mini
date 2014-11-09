4c0dd2adb5c951750201cc50f5c9fa690c736c85
Panique <panique@web.de>
2014-11-09 01:11:43 +0100

    js-AJAX-calls now work in subfolder

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,8 @@
 $(function() {
 
-    // TODO can a JS expert write this code like it should look like ?
+    // just a super-simple JS demo
 
-	var demoHeaderBox;
+    var demoHeaderBox;
 
     // simple demo to show create something via javascript on the page
     if ($('#javascript-header-demo-box').length !== 0) {
@@ -20,8 +20,8 @@ $(function() {
         $('#javascript-ajax-button').on('click', function(){
 
             // send an ajax-request to this URL: current-server.com/songs/ajaxGetStats
-            // TODO this will not work directly when using the project in a sub-folder
-            $.ajax("/songs/ajaxGetStats")
+            // "url" is defined in views/_templates/footer.php
+            $.ajax(url + "/songs/ajaxGetStats")
                 .done(function(result) {
                     // this will be executed if the ajax-call was successful
                     // here we get the feedback from the ajax-call (result) and show it in #javascript-ajax-result-box
