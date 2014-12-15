ee6836b95f3fbd7547feb004374818668b02bd50
Panique <panique@web.de>
2014-12-15 01:31:32 +0100

    add charset to PDO creation (increased security)

--- application/config/config.php
+++ application/config/config.php
@@ -51,3 +51,4 @@ define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'mini');
 define('DB_USER', 'root');
 define('DB_PASS', 'your_password');
+define('DB_CHARSET', 'utf8');
--- application/core/controller.php
+++ application/core/controller.php
@@ -34,7 +34,7 @@ class Controller
 
         // generate a database connection, using the PDO connector
         // @see http://net.tutsplus.com/tutorials/php/why-you-should-be-using-phps-pdo-for-database-access/
-        $this->db = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS, $options);
+        $this->db = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET, DB_USER, DB_PASS, $options);
     }
 
     /**
