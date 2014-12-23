b35103272f1ec8847f085d053ed7b4f746abeef9
Chris <panique@web.de>
2014-12-23 12:53:27 +0100

    page title fix

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>PHP MVC skeleton</title>
+    <title>MINI</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
