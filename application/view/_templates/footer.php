a629f24785a4fb589a4cb13e1a4297faacd3d572
panique <c.otterstein@yahoo.de>
2020-12-25 20:23:19 +0100

    [jQuery3] upgraded to jQuery 3.x

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -2,12 +2,11 @@
     <!-- backlink to repo on GitHub, and affiliate link to Rackspace if you want to support the project -->
     <div class="footer">
         Find <a href="https://github.com/panique/mini">MINI on GitHub</a>.
-        If you like the project, support it by <a href="http://tracking.rackspace.com/SH1ES">using Rackspace</a> as your hoster [affiliate link].
     </div>
 
     <!-- jQuery, loaded in the recommended protocol-less way -->
     <!-- more http://www.paulirish.com/2010/the-protocol-relative-url/ -->
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="//code.jquery.com/jquery-3.5.1.min.js"></script>
 
     <!-- define the project's URL (to make AJAX calls possible, even when using this in sub-folders etc) -->
     <script>
